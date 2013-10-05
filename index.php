bc3ae6449fa317818d43ac68d3a2d4ad3eb81833
rlibardobj <rlibardobj@gmail.com>
2013-10-05 09:44:02 -0600

    Login.
    La vista del login ha sido implementada.

--- index.php
+++ index.php
@@ -2,65 +2,40 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Respaldos BD</title>
-        <style>
-            .marcado {
-                border: 1px solid blue;
-                background-color: lightblue;
-            }
-            .desmarcado {
-                border: 1px solid blue;
-                background-color: white;
-            }
-
-            #table_light {
-                colspan: 2;
-            }
-        </style>
-        <script>
-            function marcar(obj) {
-                if (obj.checked)
-                    obj.parentNode.parentNode.className = "marcado";
-                else
-                    obj.parentNode.parentNode.className = "desmarcado";
-            }
-        </script>
+        <link rel="stylesheet" href="index.css"/>
+        <title>Administración de Bases de Datos</title>
     </head>
     <body>
-        <?php
-        $sql = "backup database transportePublico ";
-        foreach ($_GET as $key => $value) {
-            $sql.= $sql."FILE = '$key'";
-        }
-        $sql = substr($sql, 0, strlen($sql) - 2);
-        $sql.= " to disk = 'E:\BD2\\transportePublico.bak'";
-        echo $sql;
-        $serverName = "localhost"; //serverName\instanceName
-        $connectionInfo = array("Database" => "transportePublico", "UID" => "sa", "PWD" => "12345");
-        $conn = sqlsrv_connect($serverName, $connectionInfo);
-        if (!$conn) {
-            die("Could not connect to database");
-        }
-        $sql = "select name from BD_InfoKB";
-        $stmt = sqlsrv_query($conn, $sql);
-        if ($stmt === FALSE)
-            die(print_r(sqlsrv_errors(), true));
-        echo "<center>";
-        echo "<h1>Respaldo de la Base de Datos Transporte Público</h1>";
-        echo "<form>";
-        echo "<table border =1 cellspacing = 0px id = \"table_light\">";
-        $i = 0;
-        while (sqlsrv_fetch($stmt)) {
-            $id = sqlsrv_get_field($stmt, 0);
-            echo "<tr id = \"fila$i\"><td><input onclick = \"marcar(this)\" type = \"checkbox\" name=\"$id\"></td>";
-            echo "<td>$id</td></tr>";
-            $i = $i + 1;
-        }
-        echo "</table>";
-        echo "<br>";
-        echo "</form>";
-        echo "<input type = \"submit\" value = \"Ejecutar Respaldo\">";
-        echo "</center>";
-        ?>
+        <div class="contenido">
+            <div id="ingresoSistema">
+                <form method="POST">
+                    <center>
+                        <label>Ingreso al Sistema</label>
+                    </center>
+                    <br>
+                    <table align="center">
+                        <tr>
+                            <td>Servidor</td>
+                            <td><input type="input"></td>
+                        </tr>
+                        <tr>
+                            <td>Base de Datos</td>
+                            <td><input type="input"></td>
+                        </tr>
+                        <tr>
+                            <td>Usuario</td>
+                            <td><input type="input"></td>
+                        </tr>
+                        <tr>
+                            <td>Contraseña</td>
+                            <td><input type="password"></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" align="center"><input type="submit" value="Ingresar"></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+        </div>
     </body>
 </html>
