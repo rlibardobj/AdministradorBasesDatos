0c797f5c57fe7b81be8c1942d794722e1d6a3be6
rlibardobj <rlibardobj@gmail.com>
2013-10-08 16:16:47 -0600

    Clase main añadida.
    Clase main añadida junto con código preliminar.

---
+++ main.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <nav>
+            
+        </nav>
+        <div>
+            
+        </div>
+    </body>
+</html>
+
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
